62d2051875acbe8c224bd90fb0c4ad0767e010e5
John Jin <john.jin@sonymobile.com>
2014-01-13 11:29:20 +0800

    add SonyNotes

---
+++ SonyNotes/resouces/unixadminschool.com » Linux – Understand Device Mapper and DM-Multipath_files/import.config.alt.php
@@ -0,0 +1,5 @@
+//Path to FCPlayer Folder (must end in a backslash)' .
+var fc_chat_path = "http://gurkulindia.com/main/wp-content/plugins/fcchat/";
+if(!window["FCChatConfig_wp"]){
+	document.write("<script type='text/javascript' src='" + fc_chat_path + "config/config.js'></script>");
+}
