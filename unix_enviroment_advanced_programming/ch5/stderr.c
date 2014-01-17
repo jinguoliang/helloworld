4254592f2be051f93bdc692f06cff67e690394ee
John Jin <john.jin@sonymobile.com>
2014-01-17 20:05:40 +0800

    w03

---
+++ unix_enviroment_advanced_programming/ch5/stderr.c
@@ -0,0 +1,68 @@
+/*
+*filename:	stderr.c
+*author: 	Jinux
+*create:	Sat 04 Jan 2014 08:25:57 PM CST
+*提示:		真正的去享受过程,真正的去体会过程,然后,认真就来了,细心就来了,代码之美就来了,短小精悍就来了
+*/
+
+#include <errno.h>
+#include <stdarg.h>
+#include "ourhdr.h"
+
+static void err_doit(int, const char *, va_list);
+
+char *pname = NULL;
+
+void err_sys(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_quit(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(0, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_dump(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, fmt, ap);
+	va_end(ap);
+	abort();
+	exit(1);
+}
+
+void err_ret(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, fmt, ap);
+	va_end(ap);
+	return;
+}
+
+static void
+err_doit(int errnoflag,const char *fmt,va_list ap)
+{
+	int errno_save;
+	char buf[MAXLINE];
+
+	errno_save=errno;
+	vsprintf(buf,fmt,ap);
+	if(errnoflag)
+		sprintf(buf+strlen(buf),":%s",strerror(errno_save));
+	strcat(buf,"\n");
+	fflush(stdout);
+	fputs(buf,stderr);
+	fflush(NULL);
+	return;
+}
